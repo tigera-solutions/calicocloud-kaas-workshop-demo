ae605da358e56902b2cc8bbc2aef9e072bf99839
jessieziyuantang <76741191+jessieziyuantang@users.noreply.github.com>
2021-09-26 22:30:11 -0400

    d

--- EKS-cloud-demo/demo/attacker-rogue/QuarantineRogue.sh
+++
@@ -1,3 +0,0 @@
-POD=$(kubectl get pod -l app=rogue -n storefront -o jsonpath="{.items[0].metadata.name}")
-kubectl label pod $POD -n storefront quarantine=true
-echo "kubectl label pod $POD -n storefront quarantine=true"
